a5903fb5ed9f44ed868e6cc24ac6edccb3b65e1f
Nate Levesque <nate@thenaterhood.com>
2016-10-02 21:14:12 -0400

    Bump version number to 2.5.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ data_files =[
 
 
 setup(name='gscreenshot',
-    version='2.5.3',
+    version='2.5.4',
     description='Lightweight GTK frontend to scrot',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
