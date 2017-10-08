4c0dd9cca463a76d8b7c6372b2f8d0021321876d
Nate Levesque <nate@thenaterhood.com>
2017-10-08 19:30:58 -0400

    Missed a file, bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ data_files =[
 
 
 setup(name='gscreenshot',
-    version='2.9.0',
+    version='2.9.1',
     description='Lightweight GTK frontend to scrot',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
