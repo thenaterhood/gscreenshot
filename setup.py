02d070c4df7637a7ab6084ee815f93745167524f
Nate Levesque <nate@thenaterhood.com>
2017-10-10 21:26:08 -0400

    Reference gi instead of pygobject

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
     'pillow',
     # This currently throws an error. Install pygtk via your package manager.
     #'pygtk',
-    'pygobject'
+    'gi'
     ]
 
 test_requires = [
