5d160ef0565a2640cd3b5e359174f63ebd916e44
Nate Levesque <nate@thenaterhood.com>
2020-11-27 15:46:54 -0500

    update the .desktop automatically

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import errno
 import sys
 import os
+
+pkg_version='2.12.3'
 
 install_requires = [
     'pillow',
@@ -19,9 +22,41 @@ data_files = [
     ('share/menu', ['dist/menu/gscreenshot'])
     ]
 
+def build_data_files():
+    '''
+    This is somewhat of a workaround so that the data files that
+    contain a version number end up with the correct version number
+    in them since it's easy to forget to update them
+
+    This code assumes there will only ever be one file getting installed
+    at a particular path. For gscreenshot this is fine.
+    '''
+    files = []
+    for d in data_files:
+        generated_path = os.path.join('generated', d[1][0])
+        try:
+            with open(d[1][0], 'r') as infile:
+                infile_data = infile.read()
+                updated_data = infile_data.replace('%%VERSION%%', pkg_version)
+                try:
+                    os.makedirs(os.path.dirname(generated_path))
+                except (OSError, FileExistsError) as e:
+                    if not os.path.isdir(os.path.dirname(generated_path)):
+                        raise
+
+                with open(generated_path, 'w') as outfile:
+                    outfile.write(updated_data)
+            files.append((d[0], [generated_path]))
+        except UnicodeDecodeError:
+            # Just assume this is a binary file that we don't need to
+            # replace anything in
+            files.append((d[0], d[1]))
+
+    return files
+
 
 setup(name='gscreenshot',
-    version='2.12.2',
+    version=pkg_version,
     description='Lightweight GTK frontend to scrot',
     author='Nate Levesque',
     author_email='public@thenaterhood.com',
@@ -48,7 +83,7 @@ setup(name='gscreenshot',
         'gscreenshot.resources.gui.glade',
         'gscreenshot.resources.pixmaps'
         ],
-    data_files=data_files,
+    data_files=build_data_files(),
     package_data={
         '': ['*.glade', 'LICENSE', '*.png']
         }
