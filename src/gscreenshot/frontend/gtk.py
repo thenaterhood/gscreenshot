8ec02715ac8a00dc4cf8d42d5f4e0ac0b531ce36
Nate Levesque <nate@thenaterhood.com>
2021-04-04 11:56:37 -0400

    lint

--- src/gscreenshot/frontend/gtk.py
+++ src/gscreenshot/frontend/gtk.py
@@ -513,6 +513,10 @@ def main():
 
     locale_path = resource_filename('gscreenshot.resources', 'locale')
     locale.setlocale(locale.LC_ALL, '')
+
+    # I don't know what's going on with this. This call appears to exist,
+    # works fine, and seems required for glade localization to work.
+    #pylint: disable=no-member
     locale.bindtextdomain('gscreenshot', locale_path)
     gettext.bindtextdomain('gscreenshot', locale_path)
     gettext.textdomain('gscreenshot')
