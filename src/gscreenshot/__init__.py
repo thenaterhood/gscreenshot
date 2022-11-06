7a8437c3dbcc5f4e7998f3677cf6e7a595f3c407
Nate Levesque <nate@thenaterhood.com>
2022-11-05 23:50:04 -0400

    line too long

--- src/gscreenshot/__init__.py
+++ src/gscreenshot/__init__.py
@@ -259,7 +259,8 @@ class Gscreenshot(object):
 
         return supported_formats
 
-    def get_thumbnail(self, width: int, height: int, image: Image=None) -> typing.Optional[Image.Image]:
+    def get_thumbnail(self, width: int, height: int,
+                      image: Image=None) -> typing.Optional[Image.Image]:
         """
         Gets a thumbnail of either the current image, or a passed one
 
