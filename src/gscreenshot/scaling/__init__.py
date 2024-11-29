fdc41f52dbd710b4632d17650abbb0fa5066332d
Nate Levesque <nate@thenaterhood.com>
2024-11-28 23:46:56 -0500

    Additional scaling factor methods, Add scaling detection to About dialog (#213)
    * Obey environment variables for Qt and GTK for scaling detection
    * Add wlr-randr as a source for scaling factor
    * Add scaling factor detection method to the About dialog

--- src/gscreenshot/scaling/__init__.py
+++ src/gscreenshot/scaling/__init__.py
@@ -1,7 +1,8 @@
 """Utility functions for handling display scaling"""
 
-from .scaling_factor import get_scaling_factor
+from .scaling_factor import get_scaling_factor, get_scaling_method_name
 
 __all__ = [
     "get_scaling_factor",
+    "get_scaling_method_name",
 ]
