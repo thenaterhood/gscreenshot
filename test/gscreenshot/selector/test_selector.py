e3ef8df49d183d6714f3258bf70d7df281007fad
Nate Levesque <nate@thenaterhood.com>
2024-11-29 16:42:07 -0500

    Make it better (#216)
    * test
    
    * capture stderr

--- test/gscreenshot/selector/test_selector.py
+++ test/gscreenshot/selector/test_selector.py
@@ -64,3 +64,33 @@ class SelectorTest(unittest.TestCase):
         get_scaling_factor.return_value = 2
         region = self.selector.region_select()
         self.assertEqual((2, 4, 8, 12), region)
+
+    def test_rgba_hex_to_tuple_valid_with_prefix(self):
+        self.assertEqual(
+            (1.0, 0.996078431372549, 0.9921568627450981, 1.0),
+            self.selector._rgba_hex_to_decimals("#FFFEFDFF")
+        )
+
+    def test_rgba_hex_to_tuple_valid_no_prefix(self):
+        self.assertEqual(
+            (0.00392156862745098, 0.00784313725490196, 0.011764705882352941, 1.0),
+            self.selector._rgba_hex_to_decimals("010203FF")
+        )
+
+    def test_rgba_hex_to_tuple_no_alpha(self):
+        self.assertEqual(
+            (0.00392156862745098, 0.00784313725490196, 0.011764705882352941),
+            self.selector._rgba_hex_to_decimals("#010203")
+        )
+
+    def test_rgba_hex_to_alpha_bad_length(self):
+        self.assertEqual(
+            (0.8, 0.8, 0.8, 0.6),
+            self.selector._rgba_hex_to_decimals("#FFFF")
+        )
+
+    def test_rgba_hex_to_alpha_invalid(self):
+        self.assertEqual(
+            (0.8, 0.8, 0.8, 0.6),
+            self.selector._rgba_hex_to_decimals("#POTATO")
+        )
