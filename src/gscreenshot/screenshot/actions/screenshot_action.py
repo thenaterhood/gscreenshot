3e2592367d67ab4856547035d5fac651cc3ef6e9
Nate Levesque <nate@thenaterhood.com>
2025-02-23 19:21:14 -0500

    any

--- src/gscreenshot/screenshot/actions/screenshot_action.py
+++ src/gscreenshot/screenshot/actions/screenshot_action.py
@@ -4,7 +4,7 @@ base class for screenshot actions
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Generic, Optional, TypeVar, TYPE_CHECKING
+from typing import Any, Generic, Optional, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from gscreenshot.screenshot import Screenshot
@@ -19,7 +19,7 @@ class ParamClass():
 class ScreenshotAction(ABC, Generic[R]):
     """base screenshot action class"""
 
-    param_class = None
+    param_class: Any = None
 
     def __init__(self, **kwargs):
         if self.param_class:
