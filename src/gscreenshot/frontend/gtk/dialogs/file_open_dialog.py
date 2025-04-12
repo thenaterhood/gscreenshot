6636d21bdb1dd881608afb9c4961e11cfea32c46
Nate Levesque <nate@thenaterhood.com>
2025-04-12 19:31:07 -0400

    inheritance dude (file open dialog)

--- src/gscreenshot/frontend/gtk/dialogs/file_open_dialog.py
+++ src/gscreenshot/frontend/gtk/dialogs/file_open_dialog.py
@@ -13,54 +13,41 @@ from gi.repository import Gtk # type: ignore
 i18n = gettext.gettext
 
 
-class FileOpenDialog():
+class FileOpenDialog(Gtk.FileChooserNative):
     '''The 'open a file' dialog'''
-    #pylint: disable=too-many-arguments
+
     def __init__(self, default_filename=None, default_folder=None,
-        parent=None, choose_directory=False, mime_types=None,
+        parent=None, mime_types=None,
     ):
-
-        self.default_filename = default_filename
-        self.default_folder = default_folder
-        self.parent = parent
-        self._choose_directory = choose_directory
-        self._mime_types = mime_types
-
-    def run(self):
-        ''' Run the dialog'''
-        filename = self.request_file()
-
-        return filename
-
-    def request_file(self):
-        '''Run the file selection dialog'''
-
         file_filter:Gtk.FileFilter = Gtk.FileFilter()
 
-        if self._mime_types:
-            _ = [file_filter.add_mime_type(format) for format in self._mime_types]
+        if mime_types:
+            _ = [file_filter.add_mime_type(format) for format in mime_types]
 
-        chooser = Gtk.FileChooserNative(
-                transient_for=self.parent,
-                title=None,
-                action=Gtk.FileChooserAction.OPEN,
-                filter=file_filter,
-                )
+        super().__init__(
+            transient_for=parent,
+            title=None,
+            action=Gtk.FileChooserAction.OPEN,
+            filter=file_filter,
+        )
 
-        if self.default_filename is not None:
-            chooser.set_current_name(self.default_filename)
+        if default_filename is not None:
+            self.set_current_name(default_filename)
 
-        if self.default_folder is not None:
-            chooser.set_current_folder(self.default_folder)
+        if default_folder is not None:
+            self.set_current_folder(default_folder)
 
-        chooser.set_do_overwrite_confirmation(True)
+        self.set_do_overwrite_confirmation(True)
 
-        response = chooser.run()
+        self.connect("response", self._on_response)
 
-        if response in [Gtk.ResponseType.OK, Gtk.ResponseType.ACCEPT]:
-            return_value = chooser.get_filename()
-        else:
-            return_value = None
+        self.filename = None
 
-        chooser.destroy()
-        return return_value
+    def run(self):
+        super().run()
+
+        return self.filename
+
+    def _on_response(self, _, response):
+        if response == Gtk.ResponseType.ACCEPT:
+            self.filename = self.get_filename()
