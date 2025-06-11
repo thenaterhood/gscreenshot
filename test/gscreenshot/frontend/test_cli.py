0936b5156a8792b30324d59cdb55cb7af5069c31
Nate Levesque <nate@thenaterhood.com>
2025-06-11 08:45:09 -0400

    fix mocks for CLI

--- test/gscreenshot/frontend/test_cli.py
+++ test/gscreenshot/frontend/test_cli.py
@@ -1,6 +1,6 @@
 from importlib.resources import as_file, files
 import unittest
-from unittest.mock import Mock, call, mock_open
+from unittest.mock import call, mock_open
 from PIL import Image
 import mock
 
@@ -21,11 +21,13 @@ class CLITestGscreenshotCalls(unittest.TestCase):
             self.app.get_thumbnail.return_value = img
             screenshot = Screenshot(img)
 
-        self.screenshot_collection = Mock()
+        self.screenshot_collection = mock.MagicMock()
         self.screenshot_collection.cursor_current.return_value = screenshot
         self.app.get_screenshot_collection.return_value = self.screenshot_collection
 
-    def test_smoke(self):
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
+    def test_smoke(self, makedirs, fopen):
         args = get_args([])
         main(app=self.app, args=args)
         self.app.screenshot_full_display.assert_called_with(
@@ -35,7 +37,9 @@ class CLITestGscreenshotCalls(unittest.TestCase):
             overwrite=True,
         )
 
-    def test_params_fullscreen(self):
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
+    def test_params_fullscreen(self, makedirs, fopen):
         # TODO this should also test a custom pointer glyph
         args = get_args(["--delay", "5", "--pointer"])
         main(app=self.app, args=args)
@@ -46,7 +50,9 @@ class CLITestGscreenshotCalls(unittest.TestCase):
             overwrite=True,
         )
 
-    def test_params_region(self):
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
+    def test_params_region(self, makedirs, fopen):
         # TODO this should also test a custom pointer glyph
         args = get_args(["--delay", "5", "--pointer", "-s"])
         main(app=self.app, args=args)
@@ -54,8 +60,10 @@ class CLITestGscreenshotCalls(unittest.TestCase):
             delay=5, capture_cursor=True, cursor_name=None, overwrite=True
         )
 
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
     @mock.patch('src.gscreenshot.actions.subprocess.run')
-    def test_notify(self, notify):
+    def test_notify(self, notify, makedirs, fopen):
         args = get_args(["--delay", "5", "--notify"])
         main(app=self.app, args=args)
         self.app.screenshot_full_display.assert_called_with(
@@ -63,14 +71,18 @@ class CLITestGscreenshotCalls(unittest.TestCase):
         )
         notify.assert_called_once()
 
-    def test_selection_color(self):
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
+    def test_selection_color(self, makedirs, fopen):
         args = get_args(["--delay", "5", "--select-color", "#00000000", "-s"])
         main(app=self.app, args=args)
         self.app.set_select_color.assert_called_with("#00000000")
         self.app.screenshot_selected.assert_called_with(
             delay=5, capture_cursor=False, cursor_name=None, overwrite=True)
 
-    def test_selection_weight(self):
+    @mock.patch('builtins.open', new_callable=mock_open, create=True)
+    @mock.patch('os.makedirs')
+    def test_selection_weight(self, makedirs, fopen):
         args = get_args(['--select-border-weight', '20', '--select-color', '', '-s'])
         main(app=self.app, args=args)
         self.app.set_select_border_weight.assert_called_with(20)
